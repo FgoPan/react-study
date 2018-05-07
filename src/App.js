10c8b35831a29b9f0730d15681a8621168a04930
pansiyan <pansiyan2005@126.com>
2018-05-07 22:23:17 +0800

    a

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import React, {
 } from 'react';
 import QuestionForm from './components/QuestionForm.js'
 import Question from './components/Question.js'
+import AddQuestion from './components/AddQuestion.js'
 import './App.css';
 class App extends Component {
+  static defaultProps = {
+        title:'React问答',
+      btnText:'添加问题'
+    };
   render() {
     return (
       <div>
           <div className="header container-fluid">
-            <h1>React问答</h1>
-            <button className="btn btn-success pull-right">添加问题</button>
+            <AddQuestion title={this.props.title} btnText={this.props.btnText}/>
           </div>
           <div className="container-fluid">
             <div className="row">
